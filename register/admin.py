5984d30afc2df68bc2d5ed2dc8136226f7e5d012
Deep-Ace <hello.world.deepace@gmail.com>
2022-07-04 22:11:31 +0545

    For Deployment Purpose

---
+++ register/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from register.models import AddBook
+from django.utils.html import format_html
+
+# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.b_pic.url))
+
+    thumbnail.short_description = 'Book_Image'
+    list_display = ('book_id','thumbnail','b_name')
+    list_display_links = ('book_id', 'thumbnail', 'b_name')
+
+admin.site.register(AddBook, BookAdmin)
